0ca56289ef0de7b43ce685fabb963b62479c7e93
Ben Deltenre <benjdelt@gmail.com>
2021-12-19 01:29:35 -0800

    Made mobile nav menu functional

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,16 +13,39 @@ function Nav() {
   const { t, i18n } = useTranslation();
   const switchValue = i18n.language === "en" ? "fr" : "en";
 
-  const isMobileOpen = true ? styles.mobileOpen : styles.mobileClosed;
+  const wrapperRef = useRef(null);
+  const [isMobileOpen, setMobileOpen] = useState(false);
+
+  const isMobileOpenStyles = isMobileOpen ? styles.mobileOpen : styles.mobileClosed;
+
+  const handleClickOutside = event => {
+    // Close menu when click outside
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setMobileOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
+    };
+  }, [])
+
+  const handleClick = event => {
+    event.stopPropagation(); // Prevents closing menu as soon as it opens
+    setMobileOpen(!isMobileOpen);
+  };
+
 
   return (
     <nav  className={styles.navBar}>
       <div className={styles.buttonContainer}>
-        <button className={styles.mobileButton} >
+        <button className={styles.mobileButton} onClick={handleClick} >
           <FontAwesomeIcon icon={faBars} />
         </button>
       </div>
-      <ul className={`${styles.menu} ${isMobileOpen}`} >
+      <ul className={`${styles.menu} ${isMobileOpenStyles}`} onClick={() => setMobileOpen(false)} ref={wrapperRef}>
         <li><Link href="/">{ t('nav.presentation') }</Link></li>
         <li><Link href="/services">{ t('nav.services') }</Link></li>
         <li><Link href="/contact">{ t('nav.contact') }</Link></li>
