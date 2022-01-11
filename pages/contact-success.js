015820a123b51ec7e2b13ff444eb6a701006ee72
Ben Deltenre <benjdelt@gmail.com>
2022-01-11 03:15:12 -0800

    Added message sent page

--- pages/contact-success.js
+++ pages/contact-success.js
@@ -1,30 +1,24 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Section from '../components/Section';
+import LinkText from '../components/LinkText';
 import ContactForm from '../components/ContactForm';
 
 export default function Contact() {
 
   const router = useRouter();
-  const { query, locale } = router;
+  const { pathname, query, asPath, locale } = router;
 
   const { t } = useTranslation('contact');
-
-  const labels = {name: t('name'), email: t('email'), subject: t('subject'), message: t('message'), send: t('send')};
-  const errorMessages = {required: t('required'), alphabeticalError: t('alphabeticalError'), nameLengthError: t('nameLengthError'), emailFormatError: t('emailFormatError')};
-
-  useEffect(() => {
-    if (query.success) {
-      router.push({ pathname: '/contact-success', query }, '', { locale: query.locale });
-    }
-  }, [])
 
   return (
     <Section background>
       <h1>{t('contact')}</h1>
-      <ContactForm labels={labels} errorMessages={errorMessages} locale={locale}/>
+      <p>{t('messageSent')}</p>
+      <p><Link href="/">{t('backToMainPage')}</Link></p>
     </Section>
   )
 }
